982de1bb7eeff9bd035ad2f2df5a141317891919
saberprivateer <daniel.kamerling@gmail.com>
2017-04-11 17:41:40 -0700

    moar tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -2,14 +2,14 @@ import { browser, element, by } from 'protractor';
 
 describe('NannyLog E2E Tests', function () {
 
-  let expectedMsg = 'Hello Angular';
+  let expectedMsg = 'A note from Kristin and Dan...';
 
   beforeEach(function () {
     browser.get('');
   });
 
   it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+    expect(element(by.id('parent_note')).getText()).toEqual(expectedMsg);
   });
 
 });
